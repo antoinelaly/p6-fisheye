aa484bc0f633526d2eb61c34b427685e21647963
Antoine Laly <antoine.laly@gmail.com>
2021-03-12 16:43:39 +0100

    aws

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var count = document.querySelector(".out");
 
-fetch('https://raw.githubusercontent.com/aZolo77/citiesBase/master/cities.json')
+fetch('https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeDataFR.json')
 .then(response => {
   return response.json();
 }).then(data => {
