cfcfc83156e15f4c6ed8af1a7cdb385b230e1ec9
Antoine Laly <antoine.laly@gmail.com>
2021-03-12 16:35:38 +0100

    url

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var count = document.querySelector(".out");
-const url = "https://raw.githubusercontent.com/aZolo77/citiesBase/master/cities.json";
-//const url = "https://choisirsontheme.com/formation/ocr/p6-fisheye-fetch/js/fisheyedatafr.json";
+//const url = "https://raw.githubusercontent.com/aZolo77/citiesBase/master/cities.json";
+const url = "https://choisirsontheme.com/formation/ocr/p6-fisheye-fetch/js/fisheyedatafr.json";
+//const url = "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeDataFR.json";
 
 fetch(url)
 .then(response => {
@@ -15,4 +16,4 @@ fetch(url)
  count.innerHTML = out;
 }).catch(err => {
   console.log('Fetch Error :-S', err);
-});https://github.com/antoinelaly/p6-fisheye/blob/main/js/fisheyedatafr.json
+});
