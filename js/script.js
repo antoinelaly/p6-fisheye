05f3fca57bff5c96355729dad08318de474d100f
Antoine Laly <antoine.laly@gmail.com>
2021-03-13 08:26:08 +0100

    integration

--- js/script.js
+++ js/script.js
@@ -7,7 +7,24 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyen
   let out="";
   for(key in data){
     data[key].forEach(function (el){
-      out+=`<li> ${el.name} </li>`
+
+      out+=`    
+      <figure>
+      <a href="#"><img src="img/${el.portrait}" alt="${el.name}"></a>
+      <figcaption>
+        <h2><a href="#">${el.name}</a></h2>  
+        <div class="city">city${el.city}</div>
+        <div class="brand">${el.tagline}</div>
+        <div class="price">${el.price}€/jour</div>
+        <ul>
+          <li class="petitsb"><a href="#">#sport</a></li>
+          <li class="petitsb"><a href="#">#event</a></li>
+          <li class="petitsb"><a href="#">#travel</a></li>
+          <li class="petitsb"><a href="#">#animals</a></li>
+        </ul>
+      </figcaption>
+    </figure>   
+      `
     })
   } 
  count.innerHTML = out;
