eedca0af8829201c808e26cd94eeb9130ff157b9
Antoine Laly <antoine.laly@gmail.com>
2021-03-14 18:56:32 +0100

    key

--- js/script.js
+++ js/script.js
@@ -7,22 +7,24 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
 
   let out="";
   for(key in data){
-    data[key].forEach(function (el){
-      out+=`    
-      <figure>
-      <a href="#"><img src="img/${el.portrait}" alt="${el.name}"></a>
-      <figcaption>
-        <h2><a href="#">${el.name}</a></h2>  
-        <div class="city">city${el.city}</div>
-        <div class="brand">${el.tagline}</div>
-        <div class="price">${el.price}€/jour</div>
-        <ul class="lestags">
-            <li class="petitsb"><a href="#">#${el.tags}</a></li>
-        </ul>
-      </figcaption>
-    </figure>   
-      `
-    })
+    if(key == "photographers") {
+      data[key].forEach(function (el){
+        out+=`    
+        <figure>
+        <a href="#"><img src="img/${el.portrait}" alt="${el.name}"></a>
+        <figcaption>
+          <h2><a href="#">${el.name}</a></h2>  
+          <div class="city">city${el.city}</div>
+          <div class="brand">${el.tagline}</div>
+          <div class="price">${el.price}€/jour</div>
+          <ul class="lestags">
+              <li class="petitsb"><a href="#">#${el.tags}</a></li>
+          </ul>
+        </figcaption>
+      </figure>   
+        `
+      })
+    } 
   } 
  count.innerHTML = out;
 
