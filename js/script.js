bc7aeed910f3acf57db72f99725114295e3c1ff7
Antoine Laly <antoine.laly@gmail.com>
2021-03-14 16:30:30 +0100

    photographersArray

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyen
 .then(response => {
   return response.json();
 }).then(data => {
-
+  var photographersArray = data.photographers;
   let out="";
   for(key in data){
-    data[key].forEach(function (el){
+    photographersArray[key].forEach(function (el){
       out+=`    
       <figure>
       <a href="#"><img src="img/${el.portrait}" alt="${el.name}"></a>
