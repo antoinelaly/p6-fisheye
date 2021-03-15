41b7d6c9281e8b6719937c66a52d2fbb85e50926
Antoine Laly <antoine.laly@gmail.com>
2021-03-15 11:42:53 +0100

    retour

--- js/script.js
+++ js/script.js
@@ -19,7 +19,6 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
           <div class="price">${el.price}€/jour</div>
           <ul class="lestags">
               <li class="petitsb"><a href="#">#${el.tags}</a></li>
-              <li class="petitsb"><a href="#">#${el.tags}</a></li>
           </ul>
         </figcaption>
       </figure>   
