1b7c0954ba3cbcf110197bfdd2976c52b082806a
Antoine Laly <antoine.laly@gmail.com>
2021-03-15 17:47:15 +0100

    key

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
  //let mimi = _.find(data, ["photographerId", "243"]);
  //if (media.photographerId == 243) {
  for(key in data){
-  if (data.media.photographerId == 243) {
+  if (key.media.photographerId == 243) {
      data[key].forEach(function (el){
       photo+=`    
        <figure>
