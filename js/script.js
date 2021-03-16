a80a4ce9658cff06553760ada684f1e8c01ea463
Antoine Laly <antoine.laly@gmail.com>
2021-03-16 12:09:19 +0100

    loop

--- js/script.js
+++ js/script.js
@@ -29,12 +29,14 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
   } 
  count.innerHTML = out;
 
- if(media.photographerId.indexOf(243) !== -1){
-  alert("La valeur existe!")
-} else{
-  alert("La valeur n'existe pas!")
-}
-
+ for(key in data){
+  if(media.photographerId.indexOf(243) !== -1){
+    alert("La valeur existe!")
+  } else{
+    alert("La valeur n'existe pas!")
+  }
+ };
+ 
  let photo="";
  //let mimi = _.find(data, ["photographerId", "243"]);
  //if (media.photographerId == 243) {
