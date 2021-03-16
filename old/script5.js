2cac62850412b86227f5a10abfc7e20ebf6f82c6
Antoine Laly <antoine.laly@gmail.com>
2021-03-16 17:03:59 +0100

    retour

---
+++ old/script5.js
@@ -0,0 +1,37 @@
+var count = document.querySelector(".out");
+var lesphotos = document.querySelector(".lesphotos");
+var lestags = document.querySelector(".lestags");
+
+fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyedatafr.json')
+.then(response => {
+  return response.json();
+}).then(data => {
+
+  let out="";
+  for(key in data){
+    if(count && key == "photographers") {
+      data[key].forEach(function (el){
+        out+=`    
+        <figure>
+        <a href="#"><img src="img/${el.illustration}" alt="${el.name}"></a>
+        <figcaption>
+          <h2><a href="#">${el.name}</a></h2>  
+          <div class="city">${el.city}</div>
+          <div class="brand">${el.tagline}</div>
+          <div class="price">${el.price}€/jour</div>
+          <ul class="lestags">
+          <li class="petitsb"><a href="#">#${el.tags}</a></li>
+          </ul>
+        </figcaption>
+      </figure>   
+        `
+      })
+    } 
+    count.innerHTML = out;
+
+  };
+
+
+}).catch(err => {
+  console.log('Fetch Error :-S', err);
+});
