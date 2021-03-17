d3495bf4a1b3bfded7ec4a53dcc176f2c4f79be3
Antoine Laly <antoine.laly@gmail.com>
2021-03-17 21:04:01 +0100

    out

--- js/script.js
+++ js/script.js
@@ -11,26 +11,22 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
   for(key in data){
     //if(key == "photographers") {
       data["photographers"].forEach(function (el){
-        out+=`    
-        <figure>
-        <a href="https://antoinelaly.github.io/p6-fisheye/${el.id}"><img src="img/${el.illustration}" alt="${el.name}"></a>
-        <figcaption>
-          <h2><a href="#">${el.name}</a></h2>  
-          <div class="city">${el.city}</div>
-          <div class="brand">${el.tagline}</div>
-          <div class="price">${el.price}€/jour</div>
-          <ul class="lestags">
-            <li class="petitsb"><a href="#">  
-            ${[el.tags]}
-            </a></li>
-          </ul>
-        </figcaption>
-      </figure>   
-        `
+        out += "<figure>";
+        out += "<a href=\"https://antoinelaly.github.io/p6-fisheye/\"" + el.id + "\"><img src=\"img/\"" + el.illustration + "\" alt=\"" + el.name + "\"></a>" ;
+        out += "<figcaption>";
+        out += "<h2><a href=\"https://antoinelaly.github.io/p6-fisheye/\"" + el.id + "\">" + el.name + "</a></h2>";
+        out += "<div class=\"city\">" + el.city + "</div>";
+        out += "<div class=\"brand\">" + el.tagline + "</div>";
+        out += "<div class=\"price\">" + el.price + "€/jour </div>";
+        out += "<ul class=\"lestags\">";
+        out += "<li class=\"petitsb\"><a href=\"#\">" + [el.tags] + "</a></li>";
+        out += "</ul>";
+        out += "</figcaption>";
+        out += "</figure>";
       })
-    //} 
-  }
- count.innerHTML = out; // appendchild construire html 
+    }
+    count.append(out);
+ //count.innerHTML = out;  appendchild construire html 
   }
 
   function date_sort(a, b) {
