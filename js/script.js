e5d9b6627a7552458d4b3164c6f943dc692aafbe
Antoine Laly <antoine.laly@gmail.com>
2021-03-17 14:12:37 +0100

    med

--- js/script.js
+++ js/script.js
@@ -51,7 +51,8 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
           `
         })
       }     
-    lesphotos.innerHTML = med.sort(custom_sort);
+      med.sort(custom_sort)
+    lesphotos.innerHTML = med;
 
   } 
 
