73e15cac0e460bc54a4be4a297c92b1838391d60
Antoine Laly <antoine.laly@gmail.com>
2021-03-17 11:22:35 +0100

    reverse

--- js/script.js
+++ js/script.js
@@ -47,7 +47,7 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
           `
         })
       }
-    lesphotos.innerHTML = med;
+    lesphotos.innerHTML = med.reverse();
   } 
 
 }).catch(err => {
