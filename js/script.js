04c08298c5fca769f1df39598dc6d314db14c851
Antoine Laly <antoine.laly@gmail.com>
2021-03-19 12:34:56 +0100

    tags

--- js/script.js
+++ js/script.js
@@ -33,9 +33,9 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
         for (let j = 0; j < menu.length; j++) {
           let li = createNode("li");
           li.className = "petitsb"; 
-          li.innerHTML = tags;
           append(ul, li);
         }
+        li.innerHTML = `${el.tags}`;
 
         append(figure, img);
         append(figure, figcaption);
