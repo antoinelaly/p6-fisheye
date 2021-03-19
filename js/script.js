ebfdefd952a4103db6dbb0793b4fb12e649c5a8e
Antoine Laly <antoine.laly@gmail.com>
2021-03-19 10:32:36 +0100

    test

--- js/script.js
+++ js/script.js
@@ -29,6 +29,14 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
         img.src = `img/${el.illustration}`;
         p.innerHTML = `${el.city} <br>${el.tagline} <br>${el.price}€/jour`;
 
+        let menu = el.tags;
+        for (let j = 0; j < menu.length; j++) {
+          let li = createNode("li");
+          li.className = "petitsb"; 
+
+        }
+        li.innerHTML = `${el.tags}`;
+        append(ul, li);
         append(figure, img);
         append(figure, figcaption);
         append(figcaption, h2);
@@ -36,13 +44,6 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
         append(figcaption, ul);
         append(home, figure);
       })
-      let menu = el.tags;
-      for (let j = 0; j < menu.length; j++) {
-        let li = createNode("li");
-        li.className = "petitsb"; 
-        li.innerHTML = `${el.tags}`;
-        append(ul, li);
-      };
     }
 
     
