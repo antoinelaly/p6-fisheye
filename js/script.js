932179bbf7f9ee81b4797faa3c0b55c8716d76f9
Antoine Laly <antoine.laly@gmail.com>
2021-03-20 17:36:03 +0100

    folioGallery

--- js/script.js
+++ js/script.js
@@ -59,15 +59,15 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
 
   if(searchParams.has('id')) {
     let folioId = searchParams.get('id');
-    //let folioGallery = data[folioId];
-    //console.log(folioId);243
+    let folioGallery = data[folioId];
+    console.log(folioGallery);
 
   let med="";
     for(key in data) {
         data["media"].forEach(function (el){
           //console.log(folioId);
           if(el['photographerId'] === folioId)
-          console.log(folioId);
+          //console.log(folioId);
           med+=`    
           <figure>
             <a href="#"><img src="img/${el.photographerId}/${el.image}" alt="${el.name}"></a>
