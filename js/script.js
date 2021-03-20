e9bae80bd16488093b48cff6986c5c41ade6a71b
Antoine Laly <antoine.laly@gmail.com>
2021-03-20 13:25:21 +0100

    folioId

--- js/script.js
+++ js/script.js
@@ -65,8 +65,8 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
   let med="";
     for(key in data) {
         data[key].forEach(function (el){
-          if(el['photographerId'] === 243)
-          console.log(folioId);
+          if(el['photographerId'] === folioId)
+          //console.log(folioId);
           med+=`    
           <figure>
             <a href="#"><img src="img/${el.photographerId}/${el.image}" alt="${el.name}"></a>
