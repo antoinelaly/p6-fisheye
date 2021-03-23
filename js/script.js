eafd6bc1235938ef5d102f5c36b42d32256985f6
Antoine Laly <antoine.laly@gmail.com>
2021-03-23 11:31:16 +0100

    html

--- js/script.js
+++ js/script.js
@@ -78,7 +78,9 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
             (img = createNode("img")),
             (p = createNode("p"));
             img.src = `img/${el.photographerId}/${el.image}`;
-            p.innerHTML = `${el.date}  ${el.price} €  ${el.likes} &hearts;`;
+            p.innerHTML = `${el.date}`;
+            p.innerHTML = `${el.price} €`;
+            p.innerHTML = `${el.likes} &hearts;`;
             append(figure, img);
             append(figure, figcaption);
             append(figcaption, p);
