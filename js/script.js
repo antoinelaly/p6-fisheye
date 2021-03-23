4971095636aed369a663d197c8097e650ca11bc0
Antoine Laly <antoine.laly@gmail.com>
2021-03-23 14:48:28 +0100

    tags

--- js/script.js
+++ js/script.js
@@ -45,7 +45,8 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
 
       append(nav, ul);
 
-    });
+    })
+  };
 
   if(home) {
       data["photographers"].forEach(function (el){
