53a6580f4653dbc722f667e65d714f055a60569c
Antoine Laly <antoine.laly@gmail.com>
2021-03-23 11:33:42 +0100

    html

--- js/script.js
+++ js/script.js
@@ -84,6 +84,8 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
             append(figure, img);
             append(figure, figcaption);
             append(figcaption, p);
+            append(figcaption, p);
+            append(figcaption, p);
             append(lesphotos, figure);
           }
         })
