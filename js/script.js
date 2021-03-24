cfc1c3d511f98b2037e526cde4709a5877942757
Antoine Laly <antoine.laly@gmail.com>
2021-03-24 18:32:37 +0100

    tagpage

--- js/script.js
+++ js/script.js
@@ -80,39 +80,8 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
   
       if(el['tags'].includes(folioId)) { 
           
-      let ul = createNode("ul"),
-      figure = createNode("figure"),
-      figcaption = createNode("figcaption");
-      (img = createNode("img")),
-      (h2 = createNode("h2")),
-      (p = createNode("p"));
-      address = createNode("address");
-      a = createNode("a");
-      aImg = createNode("a");
-      h2.innerHTML = `${el.name}`;
-      aImg.href = `folio?id=${el.id}`;
-      img.src = `img/${el.illustration}`;
-      p.innerHTML = `${el.city} <br>${el.tagline} <br>${el.price}€/jour`;
-      ul.setAttribute("aria-label", "Secondary navigation");
-
-      for (let j = 0; j < el.tags.length; j++) {
-
-        let liTags = createNode("li");
-        (aTag = createNode("a"));
-        aTag.href = `tag?id=${el.tags[j]}`;
-        liTags.className = "petitsb"; 
-        liTags.innerHTML = `#${el.tags[j]}`;
-        append(ul, aTag);
-        append(aTag, liTags);
-      }
-
-      append(figure, aImg);
-      append(aImg, img);
-      append(figure, figcaption);
-      append(figcaption, h2);
-      append(figcaption, p);
-      append(figcaption, ul);
-      append(tagpage, figure);
+        creatFigure(el);
+        append(tagpage, figure);
     }
     })
 };
