f8898254ea52466161562201d808f09e5ba53cd7
Antoine Laly <antoine.laly@gmail.com>
2021-03-25 09:58:19 +0100

    indentation

--- js/script.js
+++ js/script.js
@@ -69,35 +69,30 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
   var folioIdNum = parseInt(folioId); // convert string to num
 
   if(home) {
-      data["photographers"].forEach(function (el, theValue){
-        var theValue = home;
-        creatFigure(el, theValue);
-        
-      })
+    data["photographers"].forEach(function (el, theValue){
+      var theValue = home;
+      creatFigure(el, theValue);
+    })
   };
 
   if(tagpage && searchParams.has('id')) {
     data["photographers"].forEach(function (el){
-  
       if(el['tags'].includes(folioId)) { 
-          
         var theValue = tagpage;
         creatFigure(el, theValue);
-
-    }
+      }
     })
-};
+  };
 
   if(nav) {
     data["photographers"].forEach(function (el){
-      
       var temp = [ ]
       dataa = el.tags.filter((el)=>{
       if(!temp.includes(el.userid)){
         temp.push(el.userid)
         return true;
       }
-      });
+    });
 
       let ul = createNode("ul");
       for (let j = 0; j < dataa.length; j++) {
@@ -140,50 +135,14 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
 
         data["photographers"].forEach(function (el){
           if(el['id'] === folioIdNum) {
-
-
             var theValue = presentation;
             creatFigure(el, theValue);
-
-            /*let ul = createNode("ul"),
-            figure = createNode("figure"),
-            button = createNode("button"),
-            figcaption = createNode("figcaption");
-            (img = createNode("img")),
-            (h1 = createNode("h1")),
-            (p = createNode("p"));
-            h1.innerHTML = `${el.name}`;
-            p.innerHTML = `${el.city}, ${el.country} <br>${el.tagline}`;
-            ul.setAttribute("aria-label", "Secondary navigation");
-
-            for (let i = 0; i < el.tags.length; i++) {
-              let liTags = createNode("li");
-              (aTag = createNode("a"));
-              aTag.href = `tag?id=${el.tags[i]}`;
-              liTags.className = "petitsb"; 
-              liTags.innerHTML = `#${el.tags[i]}`;
-              append(ul, aTag);
-              append(aTag, liTags);
-            }
-
-            img.src = `img/${el.illustration}`;
-            button.innerHTML = "Contactez-moi";
-            button.className = "push-right";
-            append(figure, figcaption);
-            append(figcaption, h1);
-            append(figcaption, p);
-            append(figcaption, ul);
-            append(figure, button);
-            append(figure, img);
-            append(presentation, figure);*/
           }
         })
 
   }     else {
       //window.location.pathname = 'folio';
   }
-
- 
 
 }).catch(err => {
   console.log('Fetch Error :-S', err);
