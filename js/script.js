86ccdf8d11a831d82a309f9f347f945cb807044f
Antoine Laly <antoine.laly@gmail.com>
2021-03-26 12:28:06 +0100

    e

--- js/script.js
+++ js/script.js
@@ -144,10 +144,14 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
         data["media"].forEach(function (el){ // in data media
           if(el['photographerId'] === folioIdNum) { // photographer id
 
+            el.sort((a, b) => { return a.likes - b.likes; });
+
+            employees.forEach((e) => {
+              var valueFolio = lesphotos;
+            creatFolio(e, valueFolio);
+          });
+
             
-            elSort = el.sort(compareValues('likes', 'desc'))
-            var valueFolio = lesphotos;
-            creatFolio(elSort, valueFolio);
           }
         })
 
