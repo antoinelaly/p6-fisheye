6245982870743e31daa70d5c25cc0697011ccb71
Antoine Laly <antoine.laly@gmail.com>
2021-04-07 18:13:26 +0200

    data

--- js/script.js
+++ js/script.js
@@ -71,7 +71,8 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
       
       mediaObj = data.media;
       photoObj = data.photographers;
-      //console.log(photoObj);
+      var dataObj = Object.assign({}, mediaObj, photoObj);
+      console.log(dataObj);
       showObj(data);
       leFiltre(data);
 
@@ -79,82 +80,17 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
       console.log('Fetch Error :-S', err);
 	});
 
-  function displayData(data) {
-   
-    var searchParams = new URLSearchParams(window.location.search);  // url get string id
-    var folioId = searchParams.get('id'); // convert to var
-    var folioIdNum = parseInt(folioId); // convert string to num
-  
-    if(home) {
-      data.photographers.forEach(function (el, valueFigure){
-        var valueFigure = home;
-        creatFigure(el, valueFigure); // homepage figures 
-      })
-    };
-    if(tagpage && searchParams.has('id')) {
-      data.photographers.forEach(el => {
-        if(el.tags.includes(folioId)) { 
-          var valueFigure = tagpage;
-          creatFigure(el, valueFigure); // tagpage figures
-        }
-      })
-    };
-    if(nav) { // nav buttons
-      data.photographers.forEach(el => {
-        var temp = [ ] // no duplication
-        dataa = el.tags.filter((el)=>{
-        if(!temp.includes(el.userid)){
-          temp.push(el.userid)
-          return true;
-          }
-        });
-        let ul = createNode("ul");
-        for (let j = 0; j < dataa.length; j++) {
-          let liTags = createNode("li");
-          (aTag = createNode("a"));
-          aTag.href = `tag?id=${dataa[j]}`;
-          liTags.className = "petitsb"; 
-          liTags.innerHTML = `#${dataa[j]}`;
-          append(ul, aTag);
-          append(aTag, liTags);
-        }
-        append(nav, ul);
-      })
-    };
-    if(searchParams.has('id')) { // if id in url, folio id 
-          data.media.forEach(el => { 
-            if(el.photographerId === folioIdNum) {
-              var valueFolio = lesphotos;
-              creatFolio(el, valueFolio);
-            }
-          })
-  
-          data.photographers.forEach(el => {
-            if(el.id === folioIdNum) {
-              var valueFigure = presentation; // photographer presentation 
-              creatFigure(el, valueFigure);
-            }
-          })
-    }     else {
-        //window.location.pathname = 'folio';
-    }
-  }
-});
-
-
 function showObj(data) { // function dans fetch retourne data
   
-  data.media.forEach(el => { // loop in media
+  data.media.forEach(el => { // loop in media 
+  //dataObj.forEach(el => {
     grapherid = el.photographerId; // objects
     limage = el.image || el.video;
-    //elvideo = el.video;
 
   class MyClass {
     constructor() {
         this.graphes_ = []; // photographer id
         this.imgs_ = []; // 
-        //this.videos_ = [];
-        //this.lemedia_ = [];
     }
 
   set graphe(value) { this.graphes_.push(value); }
@@ -162,54 +98,76 @@ function showObj(data) { // function dans fetch retourne data
   
   set img(value) { this.imgs_.push(value); }
   get img() { return this.imgs_[this.imgs_.length - 1];} 
-
-  //set video(value) { this.videos_.push(value); }
-  //get video() { return this.videos_[this.videos_.length - 1];}
-
 }
 
   const myClassInstance = new MyClass();
   myClassInstance.graphe = grapherid;
   myClassInstance.img = limage;
-  //myClassInstance.video = elvideo;
 
-  //console.log(myClassInstance.graphes_);
   console.log(myClassInstance.imgs_);
-  //console.log(limage);
-  //console.log(lemedia_);
-  
   })
 }
 
-function leFiltre(data) { // autre test
 
-  class LePhotographe {
-    constructor(photoObj) {
-      this.name = photoObj.name;
-      this.id = photoObj.id;
-      this.city = photoObj.city;
-      this.country = photoObj.country;
-      this.tags = photoObj.tags;
-      this.tagline = mediaObj.tagline;
-      this.price = mediaObj.price;
-      this.portrait = mediaObj.portrait;
-      this.illustration = mediaObj.illustration;
-    }
+function displayData(data) {
+   
+  var searchParams = new URLSearchParams(window.location.search);  // url get string id
+  var folioId = searchParams.get('id'); // convert to var
+  var folioIdNum = parseInt(folioId); // convert string to num
+
+  if(home) {
+    data.photographers.forEach(function (el, valueFigure){
+      var valueFigure = home;
+      creatFigure(el, valueFigure); // homepage figures 
+    })
+  };
+  if(tagpage && searchParams.has('id')) {
+    data.photographers.forEach(el => {
+      if(el.tags.includes(folioId)) { 
+        var valueFigure = tagpage;
+        creatFigure(el, valueFigure); // tagpage figures
+      }
+    })
+  };
+  if(nav) { // nav buttons
+    data.photographers.forEach(el => {
+      var temp = [ ] // no duplication
+      dataa = el.tags.filter((el)=>{
+      if(!temp.includes(el.userid)){
+        temp.push(el.userid)
+        return true;
+        }
+      });
+      let ul = createNode("ul");
+      for (let j = 0; j < dataa.length; j++) {
+        let liTags = createNode("li");
+        (aTag = createNode("a"));
+        aTag.href = `tag?id=${dataa[j]}`;
+        liTags.className = "petitsb"; 
+        liTags.innerHTML = `#${dataa[j]}`;
+        append(ul, aTag);
+        append(aTag, liTags);
+      }
+      append(nav, ul);
+    })
+  };
+  if(searchParams.has('id')) { // if id in url, folio id 
+        data.media.forEach(el => { 
+          if(el.photographerId === folioIdNum) {
+            var valueFolio = lesphotos;
+            creatFolio(el, valueFolio);
+          }
+        })
+
+        data.photographers.forEach(el => {
+          if(el.id === folioIdNum) {
+            var valueFigure = presentation; // photographer presentation 
+            creatFigure(el, valueFigure);
+          }
+        })
+  }     else {
+      //window.location.pathname = 'folio';
   }
-
-  class FolioFactory {
-    creat(photoObj) {
-      return new LePhotographe(photoObj);
-    }
-
-    creatFromMedia(mediaObj) {
-      const prepareMediaJson = {
-        id: mediaObj.photographerId,
-      };
-      return new LePhotographe(mediaObj);
-    }
-  }
-
- const creatFromMedia = { photographerId: 82, };
- //console.log(LePhotographe());
 }
+});
+
