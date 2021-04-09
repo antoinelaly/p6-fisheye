cb6d00269ae40de71eb7b3fee416cc9d901f3624
Antoine Laly <antoine.laly@gmail.com>
2021-04-09 10:35:23 +0200

    else

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@ function creatFolio(el, valueFolio) {
   p.innerHTML = `${el.date}`;
   pp.innerHTML = `${el.price} €    ${el.likes} &hearts;`;
   if (el.videos == undefined) { append(figure, img)}
-  else if (el.img == undefined) { append(figure, videos)} ;
+  else { append(figure, videos)} ;
 
   //append(figure, img) ; // if el.img null 
   append(figure, figcaption);
