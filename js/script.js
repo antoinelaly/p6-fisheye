05b25add1c524c46c664f7287e2a58852c5fb9a0
Antoine Laly <antoine.laly@gmail.com>
2021-04-12 13:19:41 +0200

    value

--- js/script.js
+++ js/script.js
@@ -95,7 +95,7 @@ fetch('https://raw.githubusercontent.com/antoinelaly/p6-fisheye/main/js/fisheyed
     });
   }
 
-  const selectElement = document.querySelector('.ice-cream');
+  const selectElement = document.querySelector('.les-images');
 
   selectElement.addEventListener('change', (event) => {
     const result = document.querySelector('.result');
