b5b350ebeb6dbf6cc61c94cff9e061917764731d
Antoine Laly <antoine.laly@gmail.com>
2021-04-13 15:27:15 +0200

    re

--- js/script.js
+++ js/script.js
@@ -88,69 +88,11 @@ showOption = document.querySelector('#option-selected');
 select.onchange = function() {
 
 var choice = select.value;
-  switch (choice) {
 
-    case 'likes':
-      //update('likes');
-      showOption.textContent = "likes";
-      break;
-    case 'date':
-      //update('date');
-      showOption.textContent = "date";
-      displayFolio();
-      console.log(data.media);
-      break;
-    case 'price':
-      //update('price');
-      showOption.textContent = "price";
-      break;
-  }
-
-  function displayFolio() {
-    var searchParams = new URLSearchParams(window.location.search);  // url get string id
-    var folioId = searchParams.get('id'); // convert to var
-    var folioIdNum = parseInt(folioId); // convert string to num
   
-    if(searchParams.has('id')) { // if id in url, folio id 
-      
-      ladata.forEach(el => { 
-        if(el.photographerId === folioIdNum) {
-          var valueFolio = lesphotos;
-          creatFolio(el, valueFolio);
-        }
-      })
-    }
   }
-};
 
 
-const sortByDates = () => {
-  data.media.sort((a, b) => {
-    if (a.date < b.date)
-      return -1;
-    if (a.date > b.date)
-      return 1;
-    return 0;
-  })
-};
-const sortByPrice = () => {
-  data.media.sort((a, b) => {
-    if (a.price < b.price)
-      return -1;
-    if (a.price > b.price)
-      return 1;
-    return 0;
-  })
-};
-const sortByLikes = () => {
-  data.media.sort((a, b) => {
-    if (a.likes < b.likes)
-      return -1;
-    if (a.likes > b.likes)
-      return 1;
-    return 0;
-  })
-};
 
 function displayData(data) {
    
